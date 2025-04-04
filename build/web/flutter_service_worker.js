f7e10bcee32d8d71459cb39314b24db8be023784
itsabhinavism <a.for.abhinav.2003@gmail.com>
2025-04-04 08:28:04 +0530

    save

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cbae2bf54b448069682ffc860dca40a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8d2859a295e6cb2001ab24a2b01c7699",
+"flutter_bootstrap.js": "cff23fae335f781e36a72ca877ec3508",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
